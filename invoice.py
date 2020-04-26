9aa1990b89895fde27844f7189db68a71fa5c593
mikeyraptor <63586585+mikeyraptor@users.noreply.github.com>
2020-04-26 07:56:09 -0700

    Create invoice.py

---
+++ invoice.py
@@ -0,0 +1,23 @@
+
+# making a python program to invoice orders
+
+import datetime
+import os
+import math
+
+
+print ('Today Is: ')                                                                                                    print (datetime.date.today())
+
+# Add a dictionary to track orders...
+
+
+
+
+
+
+# make a printout of all orders
+
+
+
+
+
